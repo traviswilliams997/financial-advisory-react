93c2decdc4a7e70e074f3a9e8b44400f33cac257
Travis Williams <traviswilliams997@gmail.com>
2022-09-14 17:51:05 -0500

    Edit navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from './images/logo.png'
+import {FaBars, FaTimes} from 'react-icons/fa'
 
 const Navbar = () => {
   return (
@@ -8,9 +9,18 @@ const Navbar = () => {
             <a href='/' className='logo'>
               <img src ={logo} alt='logo' />
             </a>
-            
+            <div className='hamburger'>
+                <FaBars />
+            </div>
+            <ul className='nav-menu'>
+                <li className='nav-item'>
+                    <a href='/'>Home</a>
+                    <a href='/'>About</a>
+                    <a href='/'>Testimonials</a>
+                    <a href='/'>Demo</a>
+                </li>
+            </ul>
         </nav>
-        
     </div>
   )
 }
