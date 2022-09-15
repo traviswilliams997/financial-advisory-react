0fd3ab268ecbff8eb3039017529e75ad1bff9667
Travis Williams <traviswilliams997@gmail.com>
2022-09-15 15:01:40 -0500

    Create and style about component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero'
+import About from './components/About'
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
