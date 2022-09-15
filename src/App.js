de7ccf9d37a1ef428682c39a0ee41a2598e2f205
Travis Williams <traviswilliams997@gmail.com>
2022-09-15 16:27:09 -0500

    Create and style Demo  component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero'
 import About from './components/About'
 import Testimonials from './components/Testimonials'
+import Demo from './components/Demo'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <About />
       <Testimonials />
+      <Demo />
     </div>
   );
 }
